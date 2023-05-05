1dcea8f3a992a5c6c890dfe67f096b958b64fe07
Debayan Biswas <debayanbiswas1111@gmail.com>
2023-05-06 02:14:11 +0530

    done and dusted

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
+import { useState } from "react";
+import CartProvider from "./store/CartProvider";
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
-    <div className="app">
-      <h1>Test</h1>
-    </div>
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals />
+      </main>
+      </CartProvider>
   );
 }
 
